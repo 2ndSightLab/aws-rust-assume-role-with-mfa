0320ac59d349b412f70d3013b8b68788ea3d0ac5
Teri Radichel <github@pwn.fyi>
2025-10-31 21:46:23 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+// Author: Teri Radichel
+// Organization: 2ndSightLab.com
+
 pub mod assume_role_with_mfa_trait;
 
 pub use assume_role_with_mfa_trait::{assume_role_with_mfa, ConsolePrompt, PromptForMfaDetails};
